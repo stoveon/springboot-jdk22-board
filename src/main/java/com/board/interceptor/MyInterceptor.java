5daaea449ce9519d3419ec8b8dd5808919289178
stoveon <stopluna@naver.com>
2024-07-15 22:12:09 +0900

    [MOD] Interceptor 추가

---
+++ src/main/java/com/board/interceptor/MyInterceptor.java
@@ -0,0 +1,19 @@
+package com.board.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+public class MyInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+    }
+}
