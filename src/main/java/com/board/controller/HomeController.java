a4d28811b3dd6ffde4f57cf2a8de5493156e8af4
stoveon <stopluna@naver.com>
2024-07-15 22:10:08 +0900

    [MOD] HomeController gson 추가

--- src/main/java/com/board/controller/HomeController.java
+++ src/main/java/com/board/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.board.controller;
 
+import com.google.gson.Gson;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -8,9 +9,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * <pre>
+ *     home controller
+ * </pre>
+ *
+ * @author jejeong
+ * @since 2024. 07. 01.
+ */
 @Slf4j
 @RestController
 public class HomeController {
+
+    private static final Gson GSON = new Gson();
 
     @RequestMapping(value = "/", method = {RequestMethod.GET, RequestMethod.POST})
     public ModelAndView home(HttpServletRequest request, HttpServletResponse response) {
